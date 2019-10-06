8e7483b968a1eaadb05fd8b3ef5363afcb8c3652
Victor <victorfmarques@outlook.com>
2019-10-05 23:27:14 -0300

    teste

--- _fingerprint/teste_upload.py
+++ _fingerprint/teste_upload.py
@@ -1,7 +1,7 @@
 from initialize_sensor import f
 
-
-list_content = open("teste_1.txt",'r').split("|")
+file = open("teste_1.txt",'r')
+list_content = file.read().strip().split("|")
 
 f.uploadCharacteristics(0x01,list_content)
 f.uploadCharacteristics(0x02,list_content)
