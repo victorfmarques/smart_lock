f214cc7e01ef4f9e4abc2e6e6031019c76949be9
Victor <victorfmarques@outlook.com>
2019-10-10 19:08:22 -0300

    modified:   _fingerprint/fingerprint.py 	modified:   _fingerprint/teste_upload.py

--- _fingerprint/fingerprint.py
+++ _fingerprint/fingerprint.py
@@ -1,35 +1,33 @@
 import time
-from initialize_sensor import f
 from pyfingerprint.pyfingerprint import PyFingerprint
 
-def __init__(self):
-    ## Inicializa o sensor
-    try:
-        f = PyFingerprint('/dev/ttyUSB0', 57600, 0xFFFFFFFF, 0x00000000)
 
-        if ( f.verifyPassword() == False ):
+class Fingerprint(PyFingerprint):
+
+    def __init__(self):
+        if (self.verifyPassword() == False):
             raise ValueError('Sensor desconhecido')
 
     except Exception as e:
-        print('Sensor nao inicializado!')
-        print('Exception message: ' + str(e))
-        exit(1)
+    print('Sensor nao inicializado!')
+    print('Exception message: ' + str(e))
+    exit(1)
 
 
-def registra_digital(self) extends:
+    def registra_digital(self):
     try:
-        dir_template = "/home/pi/teste_HIODE/"
+        dir_template = "/home/pi/teste_HIODE/"s
         print('Insira o dedo...')
 
         ## Aguarda a leitura do dedo
-        while (f.readImage() == False):
+        while (self.readImage() == False):
             pass
 
         ##  Converte as caracteristicas da imagem lida e as armazena no Charbuffer 1
-        f.convertImage(0x01)
+        self.convertImage(0x01)
 
         ## Verifica se o dedo ja nao existe no BD
-        result = f.searchTemplate()
+        result = self.searchTemplate()
         positionNumber = result[0]
 
         if (positionNumber >= 0):
@@ -42,29 +40,29 @@ def registra_digital(self) extends:
         print('Insira o dedo novamente...')
 
         ## Aguarda a releitura do dedo
-        while (f.readImage() == False):
+        while (self.readImage() == False):
             pass
 
         ## Converte as caracteristicas da imagem lida e as armazena no Charbuffer 2
-        f.convertImage(0x02)
+        self.convertImage(0x02)
 
         ## Compara as caracteristicas guardadas nos buffers
-        if (f.compareCharacteristics() == 0):
+        if (self.compareCharacteristics() == 0):
             raise Exception('Os dedos nao condizem')
 
         ## Cria o Template
-        f.createTemplate()
+        self.createTemplate()
 
         ## Armazena o template
-        positionNumber = f.storeTemplate()
-        characteristics = f.downloadCharacteristics(0x02)
+        positionNumber = self.storeTemplate()
+        characteristics = self.downloadCharacteristics(0x02)
 
         with open("teste.txt", "a") as arq:
             for bit in characteristics:
                 arq.write(str(bit) + "|")
             arq.close()
 
-        f.downloadImage(dir_template + str(positionNumber) + ".bmp")
+        self.downloadImage(dir_template + str(positionNumber) + ".bmp")
 
         print('Dedo cadastrado com sucesso')
         print('New template position #' + str(positionNumber))
@@ -73,36 +71,38 @@ def registra_digital(self) extends:
         print('Exception message: ' + str(e))
         exit(1)
 
-def valida_digital(self):
-    try:
-        print('Favor inserir o dedo...')
 
-        ## Aguarda a leitura do dedo
-        while (f.readImage() == False):
-            pass
+    def valida_digital(self):
+        try:
+            print('Favor inserir o dedo...')
 
-        ## converte a imagem lida e a armazena no charbuffer1
-        f.convertImage(0x01)
+            ## Aguarda a leitura do dedo
+            while (self.readImage() == False):
+                pass
 
-        ## Proucura pelo template
-        result = f.searchTemplate()
+            ## converte a imagem lida e a armazena no charbuffer1
+            self.convertImage(0x01)
 
-        positionNumber = result[0]
-        accuracyScore = result[1]
+            ## Proucura pelo template
+            result = self.searchTemplate()
 
-        if (positionNumber == -1):
-            print('Sem dados!')
-            exit(0)
-        else:
-            print('Template encontrado na pos #' + str(positionNumber))
-            #print('Pontuacao: ' + str(accuracyScore))
+            positionNumber = result[0]
+            accuracyScore = result[1]
 
-    except Exception as e:
-        print('Operacao falhou!')
-        print('Exception message: ' + str(e))
-        exit(1)
+            if (positionNumber == -1):
+                print('Sem dados!')
+                exit(0)
+            else:
+                print('Template encontrado na pos #' + str(positionNumber))
+                # print('Pontuacao: ' + str(accuracyScore))
 
-def limpa_bd(self):
-    for i in range(0, f.getTemplateCount()):
-        print("item " + str(i))
-        f.deleteTemplate(i)
+        except Exception as e:
+            print('Operacao falhou!')
+            print('Exception message: ' + str(e))
+            exit(1)
+
+
+    def limpa_bd(self):
+        for i in range(0, self.getTemplateCount()):
+            print("item " + str(i))
+            self.deleteTemplate(i)
--- _fingerprint/teste_upload.py
+++ _fingerprint/teste_upload.py
@@ -1,22 +1,31 @@
-from initialize_sensor import f
+from fingerprint import Fingerprint
 
-file = open("teste_1.txt",'r')
-list_content = file.read().strip().split("|")
-list_valid = []
+f = Fingerprint
 
-for item in list_content:
-    if item.strip():
-        try:
-            list_valid.append(int(item))
-        except ValueError:
-            pass
+def carrega_txt():
+    file = open("teste_1.txt",'r')
+    list_content = file.read().strip().split("|")
+    list_valid = []
 
-print(list_valid)
+    for item in list_content:
+        if item.strip():
+            try:
+                list_valid.append(int(item))
+            except ValueError:
+                pass
 
-f.uploadCharacteristics(0x01,list_valid)
-f.uploadCharacteristics(0x02,list_valid)
+    print(list_valid)
 
-print(f.getTemplateCount())
-print("Create Template -> " + str(f.createTemplate()))
-print("Store Template  -> " + str(f.storeTemplate()))
-print(f.getTemplateCount())
+    f.uploadCharacteristics(0x01,list_valid)
+    f.uploadCharacteristics(0x02,list_valid)
+
+    print(f.getTemplateCount())
+    print("Create Template -> " + str(f.createTemplate()))
+    print("Store Template  -> " + str(f.storeTemplate()))
+    print(f.getTemplateCount())
+
+
+def limpa_db(self):
+    print("Depois " + str(f.getTemplateCount()))
+    f.limpa_bd()
+    print("Antes "+ str(f.getTemplateCount()))
