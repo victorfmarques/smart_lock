6e393bd1315f19ad68df3edd6567e34e5e4f716b
Victor <victorfmarques@outlook.com>
2019-10-24 21:02:13 -0300

    new file:   _api/lib_requests_py/library_requests.py

---
+++ _api/lib_requests_py/library_requests.py
@@ -0,0 +1,44 @@
+import requests
+import json
+
+URL = "https://localhost:5002/api/smartlock/"
+
+
+class ApiFingerprint(object):
+    IdUsuario = 0
+    Digital = ""
+
+
+    def __init__(self, _IdUsuario, _Digital):
+        self.id_template = _IdUsuario
+
+        if isinstance(_Digital, str):
+            self.Digital = _Digital
+
+        elif isinstance(_Digital, list):
+            for bit in _Digital:
+                self.Digital = self.Digital + str(bit) + "|"
+        else:
+            raise Exception("_characteristics argument must be a str or a list.")
+
+
+    def envia_digital_api(self):
+        result = False
+
+        if ((self.IdUsuario == 0) or (self.Digital == "")):
+            raise Exception("Invalid object for request")
+        else:
+            response = requests.post(url=URL + "inserirdigital", data=self)
+            result = response.status_code == 200
+        return result
+
+
+    def recebe_digitais_api(self):
+        result = []
+
+        response = requests.get(url=URL + "obterdigitais")
+        if response.status_code == 200:
+            result = response.json()
+            print(json.dumps(result, sort_keys=True, indent=4))
+        return result
+
