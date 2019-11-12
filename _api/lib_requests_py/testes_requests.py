57c37e037428bc53d68dca87ffe57f364751a04b
Victor <victorfmarques@outlook.com>
2019-11-12 16:31:36 -0300

    modified:   _api/lib_requests_py/testes_requests.py

--- _api/lib_requests_py/testes_requests.py
+++ _api/lib_requests_py/testes_requests.py
@@ -5,5 +5,5 @@ api = library_requests.ApiFingerprint(1,"digital")
 print("CLASSE INSTANCIADA")
 
 print("EXECUCAO RECEBE_DIGITAIS_API")
-api.recebe_digitais_api()
+library_requests.recebe_digitais_api()
 print("EXECUTOU RECEBE_DIGITAIS_API")
