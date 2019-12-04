42211a70eebea37da11cd6b61f6ff53f66ccadb7
Jonathan Gonzaga <jonathan.s.gonzaga@gmail.com>
2019-12-03 21:10:45 -0300

    Integracao fingerprint lcd

--- _fingerprint/fingerprint.py
+++ _fingerprint/fingerprint.py
@@ -1,10 +1,8 @@
 import time
 from pyfingerprint.pyfingerprint import PyFingerprint
-import library_requests
+
 
 class Fingerprint(PyFingerprint):
-
-    bool_flag = False
 
     def __init__(self):
         try:
@@ -16,41 +14,45 @@ class Fingerprint(PyFingerprint):
             print('Exception message: ' + str(e))
             exit(1)
 
-    def procura_digital(self, int_buffer):
+    def procura_digital(self):
         ## Aguarda a leitura do dedo
 
-        self.bool_flag = True
         print("Insira o dedo...")
-        while self.bool_flag:
-            if self.readImage():
-                ## converte a imagem lida e a armazena no charbuffer1
-                self.convertImage(int_buffer)
-                ## Proucura pelo template
-                return self.searchTemplate()
-        else:
-            return {}
+
+        while (self.readImage() == False):
+            pass
+        #if (self.readImage() == False):
+        #    return
+
+        ## converte a imagem lida e a armazena no charbuffer1
+        self.convertImage(0x01)
+
+        ## Proucura pelo template
+        return self.searchTemplate()
 
     def registra_digital(self):
         result = False
         try:
             dir_template = "/home/pi/teste_HIODE/"
 
-            info_digital = self.procura_digital(0x01)
+            info_digital = self.procura_digital()
             positionNumber = info_digital[0]
 
             if (positionNumber >= 0):
                 print('Template ja existente #' + str(positionNumber))
-                return result
+                exit(0)
 
             print('Remova o dedo...')
             time.sleep(1)
 
-            info_digital = self.procura_digital(0x02)
-            positionNumber = info_digital[0]
+            print('Insira o dedo novamente...')
 
-            if (positionNumber >= 0):
-                print('Template ja existente #' + str(positionNumber + 1))
-                return result
+            ## Aguarda a releitura do dedo
+            while (self.readImage() == False):
+                pass
+
+            ## Converte as caracteristicas da imagem lida e as armazena no Charbuffer 2
+            self.convertImage(0x02)
 
             ## Compara as caracteristicas guardadas nos buffers
             if (self.compareCharacteristics() == 0):
@@ -58,31 +60,19 @@ class Fingerprint(PyFingerprint):
 
             ## Cria o Template
             self.createTemplate()
+            result = True
             ## Armazena o template
-            positionNumber = self.storeTemplate()
-            characteristics = self.downloadCharacteristics(0x02)
+#            positionNumber = self.storeTemplate()
+#            characteristics = self.downloadCharacteristics(0x02)
 
-# ===================================== INSERCAO COM TXT
 #            with open("teste.txt", "a") as arq:
 #                for bit in characteristics:
 #                    arq.write(str(bit) + "|")
 #                arq.close()
 
-# ===================================== INSERCAO API
-            print(characteristics)
-            str_characteristics = ""
-            for bit in characteristics:
-                str_characteristics = str_characteristics + (str(bit) + "|")
+#            self.downloadImage(dir_template + str(positionNumber) + ".bmp")
 
-            print(str_characteristics)
-            print(positionNumber+1)
-            digital_api = library_requests.ApiFingerprint(positionNumber+1, str_characteristics)
-            result = library_requests.envia_digital_api(digital_api)
-            if not result:
-                self.deleteTemplate(positionNumber)
-                print('falha ao cadastrar dedo, tente novamente!')
-            else:
-                print('Dedo cadastrado com sucesso')
+            print('Dedo cadastrado com sucesso')
 
         except Exception as e:
             print('Operation failed!')
@@ -96,7 +86,7 @@ class Fingerprint(PyFingerprint):
         try:
             print('Favor inserir o dedo...')
 
-            info_digital = self.procura_digital(0x01)
+            info_digital = self.procura_digital()
 
             positionNumber = info_digital[0]
             accuracyScore = info_digital[1]
@@ -134,37 +124,3 @@ class Fingerprint(PyFingerprint):
         for i in range(0, self.getTemplateCount()):
             print("item " + str(i))
             self.deleteTemplate(i)
-
-    def dump_bd(self):
-
-        list_digitais = []
-        try:
-            list_digitais = library_requests.recebe_digitais_api()
-            for digital in list_digitais:
-                digital_api = library_requests.ApiFingerprint\
-                    (
-                        _id=digital["id"],
-                        _digital=str(digital["digital"])
-                    )
-
-                list_valid = []
-
-                for item in digital_api.digital.split("|"):
-                    if item.strip(""):
-                        try:
-                            list_valid.append(int(item))
-                        except ValueError:
-                            pass
-
-                print(list_valid)
-
-                self.uploadCharacteristics(0x01, list_valid)
-                self.uploadCharacteristics(0x02, list_valid)
-
-                print(self.getTemplateCount())
-                print("Create Template -> " + str(self.createTemplate()))
-                print("Store Template  -> " + str(self.storeTemplate()))
-                print(self.getTemplateCount())
-
-        except Exception as e:
-            print("Exception message: " + str(e))
